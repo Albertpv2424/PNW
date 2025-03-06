6c6d8d4348fdf024a2b4898dc945e416e07b0590
WithPau <paudomec@alumnes.ilerna.com>
2025-03-06 15:46:43 +0100

    alo

--- odds-api-laravel/database/migrations/2025_03_05_182929_crear_usuaris.php
+++ odds-api-laravel/database/migrations/2025_03_05_182929_crear_usuaris.php
@@ -3,19 +3,20 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up() {
         Schema::create('usuaris', function (Blueprint $table) {
             $table->string('nick', 50)->primary();
             $table->string('email')->unique();
-            $table->string('tipus_acc');
+            $table->enum('tipus_acc', ['Usuari', 'Usuari_premium', 'Administrador']);
             $table->string('pswd');
             $table->decimal('saldo', 10, 2)->default(0);
-            $table->date('creat_at');
+            $table->date('creat_at')->default(DB::raw('CURRENT_DATE'));
             $table->date('actualitzat_el')->nullable();
             $table->integer('apostes_realitzades')->default(0);
-            $table->integer('temps_diari')->default(0);
+            $table->integer('temps_diari')->default(3600); 
             $table->boolean('bloquejat')->default(false);
         });
     }
